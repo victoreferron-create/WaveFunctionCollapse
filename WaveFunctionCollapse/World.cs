14a9c9ac7ff038a3a54309dea067fe2422c61d27
victoreferron-create <victor.e.ferron@gmail.com>
2026-07-21 09:42:40 -0400

    created WfcTile class outline and added TileTypes static class

--- WaveFunctionCollapse/World.cs
+++ WaveFunctionCollapse/World.cs
@@ -6,7 +6,17 @@
         Beach,
         Sea,
         Forest,
-        None // default if generation algorithm fails to find the good tile type for a space or if the world has been initialized using the World(sizeX, sizeY) constructor
+        None, // default if generation algorithm fails to find the good tile type for a space or if the world has been initialized using the World(uint, uint) constructor
+    }
+
+    public static class TileTypes
+    {
+        public static readonly TileType[] Any = [
+            TileType.Sea,
+            TileType.Beach,
+            TileType.Plains,
+            TileType.Forest
+            ];
     }
 
     public class World
@@ -33,6 +43,32 @@
         public TileType GetTileAt(uint x, uint y)
         {
             return grid[x, y];
+        }
+    }
+
+    public class WfcTile
+    {
+        private uint x;
+        private uint y;
+        private TileType[] neigbours;
+        private TileType[] possibleStates = TileTypes.Any;
+
+        public WfcTile(uint x, uint y, World world)
+        {
+            this.x = x;
+            this.y = y;
+            neigbours = [
+                world.GetTileAt(x - 1, y),
+                world.GetTileAt(x + 1, y),
+                world.GetTileAt(x, y + 1),
+                world.GetTileAt(x, y - 1)
+                ];
+        }
+
+        public TileType CollapseTileType(long seed)
+        {
+            // implement method to get the tile type of the tile
+            return TileType.None;
         }
     }
 }
