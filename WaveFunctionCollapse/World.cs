581ab638073d9a58ede89e776e6e4004ee2c7db8
victoreferron-create <victor.e.ferron@gmail.com>
2026-07-20 21:31:28 -0400

    Created outline for World class

---
+++ WaveFunctionCollapse/World.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+
+namespace WaveFunctionCollapse
+{
+    public enum TileType
+    {
+        Plains,
+        Beach,
+        Sea,
+        DeepSea,
+        Mountain,
+        None // default if generation algorithm fails to find the good tile type for a space or if the world has been initialized using the World(sizeX, sizeY) constructor
+    }
+
+    public class World
+    {
+        private TileType[,] grid;
+
+        public World(long seed, uint sizeX, uint sizeY)
+        {
+            // TODO: create constructor to geneate a world using the seed provided in the seed parameter
+        }
+
+        public World(uint sizeX, uint sizeY)
+        {
+            // TODO: create constructor to generate an empty world
+        }
+
+        public TileType GetTileAt(uint x, uint y)
+        {
+            return grid[x, y];
+        }
+    }
+}
