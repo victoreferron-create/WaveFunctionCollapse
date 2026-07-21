11c9a803559831b3379ea808a86ffeef90175cba
victoreferron-create <victor.e.ferron@gmail.com>
2026-07-21 14:11:03 -0400

    Finished implementing WfcTile class

--- WaveFunctionCollapse/World.cs
+++ WaveFunctionCollapse/World.cs
@@ -1,4 +1,7 @@
-﻿namespace WaveFunctionCollapse
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace WaveFunctionCollapse
 {
     public enum TileType
     {
@@ -21,6 +24,9 @@
 
     public class World
     {
+        public int sizeX;
+        public int sizeY;
+
         private TileType[,] grid;
 
         public World(long seed, uint sizeX, uint sizeY)
@@ -51,24 +57,80 @@
         private uint x;
         private uint y;
         private TileType[] neigbours;
-        private TileType[] possibleStates = TileTypes.Any;
+        private List<TileType> possibleStates = new List<TileType>(TileTypes.Any);
+        private byte entropy;
+        private World world;
+        private bool collapsed = false;
 
         public WfcTile(uint x, uint y, World world)
         {
             this.x = x;
             this.y = y;
-            neigbours = [
-                world.GetTileAt(x - 1, y),
-                world.GetTileAt(x + 1, y),
-                world.GetTileAt(x, y + 1),
-                world.GetTileAt(x, y - 1)
-                ];
+            this.world = world;
+
+            neigbours = GetNeigbours();
+
+            entropy = (byte)possibleStates.Count;
         }
 
-        public TileType CollapseTileType(long seed)
+        public TileType CollapseTileType(Random rand)
         {
-            // implement method to get the tile type of the tile
-            return TileType.None;
+
+            int tileTypeIndex = rand.Next(possibleStates.Count);
+            TileType chosenTileType = possibleStates[tileTypeIndex];
+
+            possibleStates = new List<TileType> { chosenTileType };
+            entropy = 1;
+            collapsed = true;
+
+            return chosenTileType;
+        }
+
+        public void Update()
+        {
+
+            neigbours = GetNeigbours();
+
+            foreach (TileType type in neigbours)
+            {
+                switch (type)
+                {
+                    case (TileType.Sea):
+                        possibleStates.Remove(TileType.Plains);
+                        possibleStates.Remove(TileType.Forest);
+                        break;
+                    case (TileType.Beach):
+                        possibleStates.Remove(TileType.Forest);
+                        break;
+                    case (TileType.Plains):
+                        possibleStates.Remove(TileType.Sea);
+                        break;
+                    case (TileType.Forest):
+                        possibleStates.Remove(TileType.Beach);
+                        possibleStates.Remove(TileType.Sea);
+                        break;
+                    case (TileType.None):
+                        break;
+                    default:
+                        Console.WriteLine("What?");
+                        break;
+                }
+            }
+
+            entropy = (byte)possibleStates.Count;
+        }
+
+        public byte Entropy => entropy;
+        public bool Collapsed => collapsed;
+
+        private TileType[] GetNeigbours()
+        {
+            TileType left = (x > 0) ? world.GetTileAt(x - 1, y) : TileType.None;
+            TileType right = (x < world.sizeX - 1) ? world.GetTileAt(x + 1, y) : TileType.None;
+            TileType up = (y < world.sizeY - 1) ? world.GetTileAt(x, y + 1) : TileType.None;
+            TileType down = (y > 0) ? world.GetTileAt(x, y - 1) : TileType.None;
+
+             return [left, right, up, down];
         }
     }
 }
