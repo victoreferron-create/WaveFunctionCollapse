e071a7e3d1f80023ab0f12e6ec0b2f9819335363
victoreferron-create <victor.e.ferron@gmail.com>
2026-07-21 14:17:35 -0400

    Removed unused imports (Why does visual studio keep automatically importing stuff?)

--- WaveFunctionCollapse/World.cs
+++ WaveFunctionCollapse/World.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
-
-namespace WaveFunctionCollapse
+﻿namespace WaveFunctionCollapse
 {
     public enum TileType
     {
