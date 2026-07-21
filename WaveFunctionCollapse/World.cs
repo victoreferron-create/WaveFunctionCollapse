2ea9a72e4d195a5180ce3f2af60100012d07f2ca
victoreferron-create <victor.e.ferron@gmail.com>
2026-07-21 07:25:45 -0400

    Implement empty world constructor

--- WaveFunctionCollapse/World.cs
+++ WaveFunctionCollapse/World.cs
@@ -5,8 +5,7 @@
         Plains,
         Beach,
         Sea,
-        DeepSea,
-        Mountain,
+        Forest,
         None // default if generation algorithm fails to find the good tile type for a space or if the world has been initialized using the World(sizeX, sizeY) constructor
     }
 
@@ -21,7 +20,14 @@
 
         public World(uint sizeX, uint sizeY)
         {
-            // TODO: create constructor to generate an empty world
+            grid = new TileType[sizeX, sizeY];
+            for (int x = 0; x < sizeX; x++) 
+            {
+                for (int y = 0; y < sizeY; y++) 
+                {
+                    grid[x, y] = TileType.None;
+                }
+            }
         }
 
         public TileType GetTileAt(uint x, uint y)
